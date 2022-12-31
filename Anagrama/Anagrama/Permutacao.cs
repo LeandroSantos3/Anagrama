806250f65464636d9e224bc28876e8701ee7b43a
Leo_3 <leandro00santos.ls@gmail.com>
2022-12-31 00:23:13 +0000

    Terminado 1,2
    Ponto 1 e 2 funcionais
    
    Melhorias: pensar em usar listas, carrego as permutações para a lista e no GUI faço o seu uso

--- Anagrama/Anagrama/Permutacao.cs
+++ Anagrama/Anagrama/Permutacao.cs
@@ -1,72 +1,100 @@
-﻿/*
+﻿ ﻿/*
  * Created by SharpDevelop.
  * User: leand
  * Date: 23/12/2022
  * Time: 18:13
  * 
  */
-// C# program to implement
-// the above approach
-// C# program to implement
-// the above approach
 using System;
 using System.Collections.Generic;
+//using System.Linq;
 
 namespace Anagrama
 {
 	public class Permutacao{
-	   
-static void permute(String s,
-                    String answer)
-{   
-  
+		
+		public int permutationCount = 0;
+		private int Result 
+		{ 	get { return permutationCount; }
+		 	set {permutationCount = value;}}
+		
+public void PermutacaoComRepeticoes(String s, String answer)
+{	
+//	List<String> lstPermutacao = new List<String>();
+	
     if(s.Length==0){
-    	Console.Write(answer + "  ");    	
-    }
-    
-      
+			
+    		Console.Write(answer + "  ");
+    		//lstPermutacao.Add(answer);
+			Result++;			
+    		return;   	   	
+    }			
+     
     for(int i = 0 ;i < s.Length; i++)
-    {
+    {    	
         char ch = s[i];
         String left_substr = s.Substring(0, i);
         String right_substr = s.Substring(i + 1);
-        String rest = left_substr + right_substr;
-        permute(rest, answer + ch);
-        
-        
-    }
+        String rest = left_substr + right_substr;  
+        PermutacaoComRepeticoes(rest, answer + ch);             
+    }  
     
 }
 		
- public void PermutacaoRepeticoes(String palavra )
- {
- 	String resposta="";
-      
-    Console.Write("\n Anagrama com repiticoes :\n ");
-    permute(palavra, resposta);
-				
-}
- 
- public void PermutacaoSemRepeticoes(String palavra )
- {
-    String resposta="";
-      
-    Console.Write("\n Anagrama sem repiticoes :\n ");
-    permute(palavra, resposta);    
-				
-}
- 
- public int Contar(String palavra)
- {
- 	int resul =1;
- 	for(int i = 1; i<palavra.Length; i++)
- 	{
- 		resul = resul *i;
-	}
- 	return resul;
- }
 
+public void PermutacaoSemRepeticoes(String s, String answer, List<String>lista) {
+   
+	if (s.Length == 0){
+		
+	if(!lista.Contains(answer)){
+	   	Console.Write(answer + "    ");
+        Result++;
+        lista.Add(answer);
+        return;
+	}     
+}
   
+  for (int i = 0; i < s.Length; i++)
+    {
+  		char ch = s[i];
+        String left_substr = s.Substring(0, i);
+        String right_substr = s.Substring(i + 1);
+        String rest = left_substr + right_substr;  
+        PermutacaoSemRepeticoes(rest, answer + ch,lista );
+					
+  	} 
+  }
 
+
+/// <summary>
+/// Metod
+/// </summary>
+ public void Contar()
+ { 
+ 	
+ 	int result =permutationCount ;
+ 	Console.WriteLine("\nGerou {0}",result);
+ 	Result=0; // senao sempre é ta continjua ta conta e soma
+ 	return;
+ 	
+ }
+	
+ 
+ /// <summary>
+	/// Metodo que permita fazer a validacao para saber se a palavra esta compreendido entre 3 ~ 11 caracteres 
+	/// </summary>
+	/// <param name="palavra">string que sera introduzido pelo user</param>
+	/// <returns>se for menor que 3 ou maior que 10 return false </3></returns>se></returns>se></returns>
+	 public bool Validar(String palavra) 
+	 {
+			if(palavra.Length<3 || palavra.Length>=11)
+			{
+				Console.WriteLine("A palavra deve estar entre 3~10 caracteres");
+				return false; 
+			}
+				
+			return true;
+	}
 }
 }
+ 
--- Anagrama/Anagrama/Program.cs
+++ Anagrama/Anagrama/Program.cs
@@ -9,72 +9,68 @@ using System;
 using System.Collections.Generic;
 
 namespace Anagrama
-{	
- class Program
-	{private static string[] lstMenu = { 
- 		"Com Repeticoes     ",
- 		"Sem Repeticoes ",
-		"Por fazer...     ",
-		"Por fazer... ",
-		"SAIR" ,
+{
+	class Program
+	{
+private static string[] lstMenu = { 
+			"Com Repeticoes     ",
+			"Sem Repeticoes ",
+			"Por fazer...     ",
+			"Por fazer... ",
+			"SAIR",
 		
-								};			
-	// Bloco principal do programa
-	public static void Main(string[] args)
+		};
+		// Bloco principal do programa
+		public static void Main(string[] args)
 		{
 			Menu menu = new Menu(lstMenu);
-		 	char opcao;
-		 	Permutacao perm = new Permutacao();
+			char opcao;
+			Permutacao perm = new Permutacao();
+			List<String> lstPermutacao = new List<String>();
 		 
-	     do
-		  {
-     		opcao=menu.MenuSimples();		   
-		   	switch (opcao) {
-		 	  case '1':
-		   		Console.WriteLine("Teste do anagrama, com repeticoes");
-		   		Console.Write("Insira a palavra para fazer o jogo: ");
-		   		String palavra = Console.ReadLine();
-		   		if(Validar(palavra)==true){
-		   			perm.PermutacaoRepeticoes(palavra);
-		   			//perm.Contar();
-		   		}
-		   		else{
-		   			Console.WriteLine("A palavra deve estar entre 3~10 caracteres");
-		   		}	   		
+			do {
+				opcao = menu.MenuSimples();		   
+				switch (opcao) {
+					case '1':
+						Console.WriteLine("Teste do anagrama, com repeticoes");
+						Console.Write("Insira a palavra para fazer o jogo: ");
+						String palavra = Console.ReadLine();		   		
 		   		
-		 			   break;
-		 	  case '2':
-		 			   Console.WriteLine("2");
-		 			   break;
-		 	  case '3':
+		   		
+						if (perm.Validar(palavra) == true) {
+							Console.Write("\n Anagrama com repiticoes :\n ");		   			  			
+							perm.PermutacaoComRepeticoes(palavra, "");		   			
+							perm.Contar();
+						}
+						break;
+					case '2':
+						Console.WriteLine("Teste do anagrama, sem repeticoes");
+						Console.Write("Insira a palavra para fazer o jogo: ");
+						String palavra1 = Console.ReadLine();		   		
+		   		
+						if (perm.Validar(palavra1) == true) {
+							Console.Write("\n Anagrama sem repiticoes :\n ");		   			  			
+							perm.PermutacaoSemRepeticoes(palavra1, "",new List<String>());
+							perm.Contar();
+						}
+						break;
+					case '3':
 						Console.WriteLine("3...");
-		 			   break;		 			   
-		 	  case '4':
-		 			   Console.WriteLine("4...");
-		 			   break;	 
-			  case '5':
-		 			   Console.WriteLine("A sair...");
-		 			   break;			 			   
+						break;		 			   
+					case '4':
+						Console.WriteLine("Por fazer...");
+						break;	 
+					case '5':
+						Console.WriteLine("A sair...");
+						break;			 			   
 		 	 
-		 	}
-           if (opcao!='5') Menu.TeclaParaContinuar();
-          }while (opcao!='5');		 
-	     Console.WriteLine("\nPrograma a encerrar...");
-		 Console.ReadKey(true);
-		}	
-	
-	/// <summary>
-	/// Metodo que permita fazer a validacao para saber se a palavra esta compreendido entre 3 ~ 11 caracteres 
-	/// Faz sentido para mim ter o metodo aqui e não na class Permutacao prq deve ser validada antes de sair para o metodo
-	/// </summary>
-	/// <param name="palavra">string que sera introduzido pelo user</param>
-	/// <returns>se for menor que 3 ou maior que 10 return false </3></returns>se></returns>se></returns>
-	 public static bool Validar(String palavra) 
-	 {
-			
-			if(palavra.Length<3 || palavra.Length>=11)
-				return false;
-			return true;
+				}
+				if (opcao != '5')
+					Menu.TeclaParaContinuar();
+			} while (opcao != '5');		 
+			Console.WriteLine("\nPrograma a encerrar...");
+			Console.ReadKey(true);
 		}
-	} 
+	}
 }
+
